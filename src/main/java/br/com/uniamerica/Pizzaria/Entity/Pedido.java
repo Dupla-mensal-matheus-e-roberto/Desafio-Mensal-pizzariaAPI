6a159c56358471cd59f52a1fa60085b915d5398a
roberto <rvpassos@hotmail.com>
2023-08-15 19:39:22 -0300

    Entidade de Pedido

--- src/main/java/br/com/uniamerica/Pizzaria/Entity/Pedido.java
+++ src/main/java/br/com/uniamerica/Pizzaria/Entity/Pedido.java
@@ -1,4 +1,37 @@
 package br.com.uniamerica.Pizzaria.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pedidos", schema = "public")
+@Getter
+@Setter
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pedido")
+    private Long idPedido;
+    @Column(name = "data_do_pedido")
+    private LocalDateTime dataDoPedido;
+    @Column(name = "status")
+    private String status;
+    @Column(name = "id_cliente")
+    private Long idCliente;
+    @Column(name = "id_produto")
+    private Long idProduto;
+
+    public Pedido() {
+    }
+
+    public Pedido(Long idPedido, LocalDateTime dataDoPedido, String status, Long idCliente, Long idProduto) {
+        this.idPedido = idPedido;
+        this.dataDoPedido = dataDoPedido;
+        this.status = status;
+        this.idCliente = idCliente;
+        this.idProduto = idProduto;
+    }
 }
