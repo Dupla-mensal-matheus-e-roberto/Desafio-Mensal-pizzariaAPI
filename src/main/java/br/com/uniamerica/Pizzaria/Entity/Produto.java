ec486ffe5208b59135fa1e9befef3daf15b67fa5
roberto <rvpassos@hotmail.com>
2023-08-15 19:40:25 -0300

    Entidade de Produto

--- src/main/java/br/com/uniamerica/Pizzaria/Entity/Produto.java
+++ src/main/java/br/com/uniamerica/Pizzaria/Entity/Produto.java
@@ -1,4 +1,29 @@
 package br.com.uniamerica.Pizzaria.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "produtos", schema = "public")
+@Getter
+@Setter
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_produto")
+    private Long idProduto;
+    @Column(name = "id_pizza")
+    private Long idPizza;
+    @Column(name = "acompanhamentos")
+    private String acompanhamentos;
+
+    public Produto() {
+    }
+
+    public Produto(Long idProduto, Long idPizza, String acompanhamentos) {
+        this.idProduto = idProduto;
+        this.idPizza = idPizza;
+        this.acompanhamentos = acompanhamentos;
+    }
 }
