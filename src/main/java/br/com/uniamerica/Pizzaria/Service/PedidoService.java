6409b3c4903aab66ecf328b09b65eb68df3e48a8
roberto <rvpassos@hotmail.com>
2023-08-16 20:36:03 -0300

    Configurando Services

--- src/main/java/br/com/uniamerica/Pizzaria/Service/PedidoService.java
+++ src/main/java/br/com/uniamerica/Pizzaria/Service/PedidoService.java
@@ -1,4 +1,82 @@
 package br.com.uniamerica.Pizzaria.Service;
 
+import br.com.uniamerica.Pizzaria.DTO.PedidoDTO;
+import br.com.uniamerica.Pizzaria.Entity.Pedido;
+import br.com.uniamerica.Pizzaria.Repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PedidoService {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    private Pedido pedido;
+    private PedidoDTO pedidoDTO;
+
+    public List<PedidoDTO> getAll(){
+        List<Pedido> listBanco = this.pedidoRepository.findAll();
+        List<PedidoDTO> listDTO = new ArrayList<>();
+
+        for(int i = 0; i < listBanco.size(); i++){
+            listDTO.add(toPedidoDto(listBanco.get(i)));
+        }
+
+        return listDTO;
+    }
+
+    public PedidoDTO findById(Long id){
+        Pedido pedidoBanco = this.pedidoRepository.findById(id).orElse(null);
+
+        Assert.isTrue(pedidoBanco != null, "Pedido Inválido");
+
+        return toPedidoDto(pedidoBanco);
+    }
+    public void criar(PedidoDTO pedidoDTO){
+        pedido = toPedido(pedidoDTO);
+
+        this.pedidoRepository.save(pedido);
+    }
+
+    public void editar(PedidoDTO pedidoDTO, Long id){
+        pedido = toPedido(pedidoDTO);
+
+        pedido = this.pedidoRepository.findById(id).orElse(null);
+
+        Assert.isTrue(pedido != null, "Pedido Inválido");
+
+        this.pedidoRepository.save(pedido);
+    }
+
+    public void deletar(Long id){
+        pedido = this.pedidoRepository.findById(id).orElse(null);
+
+        Assert.isTrue(pedido != null, "Pedido Inválido");
+
+        this.pedidoRepository.delete(pedido);
+    }
+
+    public PedidoDTO toPedidoDto(Pedido pedido){
+        pedidoDTO = new PedidoDTO();
+        pedidoDTO.setDataDoPedido(pedido.getDataDoPedido());
+        pedidoDTO.setStatus(pedido.getStatus());
+        pedidoDTO.setIdProduto(pedido.getIdProduto());
+        pedidoDTO.setIdCliente(pedidoDTO.getIdCliente());
+
+        return pedidoDTO;
+    }
+
+    public Pedido toPedido(PedidoDTO pedidoDTO){
+        pedido = new Pedido();
+        pedido.setDataDoPedido(pedidoDTO.getDataDoPedido());
+        pedido.setStatus(pedidoDTO.getStatus());
+        pedido.setIdProduto(pedidoDTO.getIdProduto());
+        pedido.setIdCliente(pedidoDTO.getIdCliente());
+
+        return pedido;
+    }
 }
--- src/main/java/br/com/uniamerica/Pizzaria/Service/PizzaService.java
+++ src/main/java/br/com/uniamerica/Pizzaria/Service/PizzaService.java
@@ -1,4 +1,84 @@
 package br.com.uniamerica.Pizzaria.Service;
 
+import br.com.uniamerica.Pizzaria.DTO.PizzaDTO;
+import br.com.uniamerica.Pizzaria.Entity.Pizza;
+import br.com.uniamerica.Pizzaria.Repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PizzaService {
+
+    @Autowired
+    private PizzaRepository pizzaRepository;
+    private Pizza pizza;
+    private PizzaDTO pizzaDTO;
+
+    public List<PizzaDTO> getAll(){
+        List<Pizza> listBanco = this.pizzaRepository.findAll();
+        List<PizzaDTO> listDTO = new ArrayList<>();
+
+        for(int i = 0; i < listBanco.size(); i++){
+            listDTO.add(toPizzaDto(listBanco.get(i)));
+        }
+
+        return listDTO;
+    }
+
+    public PizzaDTO findById(Long id){
+        Pizza pizzaBanco = this.pizzaRepository.findById(id).orElse(null);
+
+        Assert.isTrue( pizzaBanco != null, "Pizza Inválido");
+
+        return toPizzaDto(pizzaBanco);
+    }
+
+    public void criar(PizzaDTO pizzaDTO){
+        pizza = toPizza(pizzaDTO);
+
+        this.pizzaRepository.save(pizza);
+    }
+
+    public void editar(PizzaDTO pizzaDTO, Long id){
+        pizza = toPizza(pizzaDTO);
+
+        pizza = this.pizzaRepository.findById(id).orElse(null);
+
+        Assert.isTrue(pizza != null, "Pizza Inválido");
+
+        this.pizzaRepository.save(pizza);
+    }
+
+    public void deletar(Long id){
+        pizza = this.pizzaRepository.findById(id).orElse(null);
+
+        Assert.isTrue(pizza != null, "Pizza Inválido");
+
+        this.pizzaRepository.delete(pizza);
+    }
+
+    public PizzaDTO toPizzaDto(Pizza pizza){
+        pizzaDTO = new PizzaDTO();
+        pizzaDTO.setAdicionais(pizza.getAdicionais());
+        pizzaDTO.setRemoviveis(pizza.getRemoviveis());
+        pizzaDTO.setTamanho(pizza.getTamanho());
+        pizzaDTO.setSabores(pizza.getSabores());
+
+        return pizzaDTO;
+    }
+
+    public Pizza toPizza(PizzaDTO pizzaDTO){
+        pizza = new Pizza();
+        pizza.setAdicionais(pizzaDTO.getAdicionais());
+        pizza.setRemoviveis(pizzaDTO.getRemoviveis());
+        pizza.setTamanho(pizzaDTO.getTamanho());
+        pizza.setSabores(pizzaDTO.getSabores());
+
+        return pizza;
+    }
+
 }
