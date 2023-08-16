21ee4e12f88c2f1531ff6eec6aa23eb3e46f04d1
roberto <rvpassos@hotmail.com>
2023-08-16 18:07:21 -0300

    criação de PedidoDTO

--- src/main/java/br/com/uniamerica/Pizzaria/DTO/PedidoDTO.java
+++ src/main/java/br/com/uniamerica/Pizzaria/DTO/PedidoDTO.java
@@ -1,4 +1,22 @@
 package br.com.uniamerica.Pizzaria.DTO;
 
-public class PedidoDTO {
+import java.time.LocalDateTime;
+
+public class PedidoDTO{
+    private Long idPedido;
+    private LocalDateTime dataDoPedido;
+    private String status;
+    private Long idCliente;
+    private Long idProduto;
+
+    public PedidoDTO() {
+    }
+
+    public PedidoDTO(Long idPedido, LocalDateTime dataDoPedido, String status, Long idCliente, Long idProduto) {
+        this.idPedido = idPedido;
+        this.dataDoPedido = dataDoPedido;
+        this.status = status;
+        this.idCliente = idCliente;
+        this.idProduto = idProduto;
+    }
 }
