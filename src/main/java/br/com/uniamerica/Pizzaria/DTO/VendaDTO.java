bea9d9e521c83046004e236aba50f03405f93214
roberto <rvpassos@hotmail.com>
2023-08-16 18:08:28 -0300

    criação de VendaDTO

--- src/main/java/br/com/uniamerica/Pizzaria/DTO/VendaDTO.java
+++ src/main/java/br/com/uniamerica/Pizzaria/DTO/VendaDTO.java
@@ -1,4 +1,21 @@
 package br.com.uniamerica.Pizzaria.DTO;
 
-public class VendaDTO {
+
+public class VendaDTO{
+    private Long idVenda;
+    private String tipoPagamento;
+    private Long idPedido;
+    private String tipoEntrega;
+    private Long idFuncionario;
+
+    public VendaDTO() {
+    }
+
+    public VendaDTO(Long idVenda, String tipoPagamento, Long idPedido, String tipoEntrega, Long idFuncionario) {
+        this.idVenda = idVenda;
+        this.tipoPagamento = tipoPagamento;
+        this.idPedido = idPedido;
+        this.tipoEntrega = tipoEntrega;
+        this.idFuncionario = idFuncionario;
+    }
 }
