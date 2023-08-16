76ffc4ac78d356ffd3c222b91efa079db8dd03ed
roberto <rvpassos@hotmail.com>
2023-08-16 18:07:47 -0300

    criação de PizzaDTO

--- src/main/java/br/com/uniamerica/Pizzaria/DTO/PizzaDTO.java
+++ src/main/java/br/com/uniamerica/Pizzaria/DTO/PizzaDTO.java
@@ -1,4 +1,21 @@
 package br.com.uniamerica.Pizzaria.DTO;
 
+
 public class PizzaDTO {
+    private Long idPizza;
+    private String sabores;
+    private String tamanho;
+    private String adicionais;
+    private String removiveis;
+
+    public PizzaDTO() {
+    }
+
+    public PizzaDTO(Long idPizza, String sabores, String tamanho, String adicionais, String removiveis) {
+        this.idPizza = idPizza;
+        this.sabores = sabores;
+        this.tamanho = tamanho;
+        this.adicionais = adicionais;
+        this.removiveis = removiveis;
+    }
 }
