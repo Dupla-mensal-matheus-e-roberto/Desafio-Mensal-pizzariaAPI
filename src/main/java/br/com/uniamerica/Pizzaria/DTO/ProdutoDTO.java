c5c33f81cc607aaed8abd36b427c4b297d8867da
roberto <rvpassos@hotmail.com>
2023-08-16 18:08:12 -0300

    criação de ProdutoDTO

--- src/main/java/br/com/uniamerica/Pizzaria/DTO/ProdutoDTO.java
+++ src/main/java/br/com/uniamerica/Pizzaria/DTO/ProdutoDTO.java
@@ -1,4 +1,17 @@
 package br.com.uniamerica.Pizzaria.DTO;
 
-public class ProdutoDTO {
+
+public class ProdutoDTO{
+    private Long idProduto;
+    private Long idPizza;
+    private String acompanhamentos;
+
+    public ProdutoDTO() {
+    }
+
+    public ProdutoDTO(Long idProduto, Long idPizza, String acompanhamentos) {
+        this.idProduto = idProduto;
+        this.idPizza = idPizza;
+        this.acompanhamentos = acompanhamentos;
+    }
 }
