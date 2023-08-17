516c0dfd7e41137f325a73fe79c6abad22121b66
roberto <rvpassos@hotmail.com>
2023-08-17 18:41:49 -0300

    endpoints definidos

--- src/main/java/br/com/uniamerica/Pizzaria/Controller/VendaController.java
+++ src/main/java/br/com/uniamerica/Pizzaria/Controller/VendaController.java
@@ -1,4 +1,66 @@
 package br.com.uniamerica.Pizzaria.Controller;
 
+import br.com.uniamerica.Pizzaria.DTO.VendaDTO;
+import br.com.uniamerica.Pizzaria.Service.VendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/venda")
 public class VendaController {
+    @Autowired
+    private VendaService vendaService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<VendaDTO>> getAll(){
+        try{
+            return ResponseEntity.ok(this.vendaService.getAll());
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<VendaDTO> findById(@PathVariable("id") Long id){
+        try{
+            return ResponseEntity.ok(this.vendaService.findById(id));
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<String> criar(@RequestBody VendaDTO vendaDTO){
+        try{
+            this.vendaService.criar(vendaDTO);
+            return ResponseEntity.ok("Venda realizada com sucesso");
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<String> editar(@RequestBody VendaDTO vendaDTO, @PathVariable("id") Long id){
+        try{
+            this.vendaService.editar(vendaDTO, id);
+            return ResponseEntity.ok("Venda editada com sucesso");
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletar(@PathVariable("id") Long id){
+        try{
+            this.vendaService.deletar(id);
+            return ResponseEntity.ok("Venda deletada com sucesso");
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
 }
