89d964dc04b39bf60396b64ed05e1506e769f23d
Tokram <xtokram@gmail.com>
2023-09-19 13:57:17 -0300

    Inicio novos tests

--- src/test/java/br/com/uniamerica/pizzaria/PizzariaApplicationTests.java
+++ src/test/java/br/com/uniamerica/pizzaria/PizzariaApplicationTests.java
@@ -2,11 +2,14 @@ package br.com.uniamerica.pizzaria;
 
 import br.com.uniamerica.pizzaria.controller.ClienteController;
 import br.com.uniamerica.pizzaria.controller.FuncionarioController;
+import br.com.uniamerica.pizzaria.controller.PedidoController;
 import br.com.uniamerica.pizzaria.dto.ClienteDTO;
 import br.com.uniamerica.pizzaria.dto.FuncionarioDTO;
+import br.com.uniamerica.pizzaria.dto.PedidoDTO;
 import br.com.uniamerica.pizzaria.entity.*;
 import br.com.uniamerica.pizzaria.repository.ClienteRepository;
 import br.com.uniamerica.pizzaria.repository.FuncionarioRepository;
+import br.com.uniamerica.pizzaria.repository.PedidoRepository;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -30,27 +34,48 @@ class PizzariaApplicationTests {
 	FuncionarioRepository funcionarioRepository;
 	@Autowired
 	FuncionarioController funcionarioController;
+	@MockBean
+	PedidoRepository pedidoRepository;
+	@Autowired
+	PedidoController pedidoControler;
 
 	@BeforeEach
 	void injectData(){
 		List<Pedido> pedidos = new ArrayList<>();
 		List<Venda> vendas = new ArrayList<>();
+		List<Produto> produtos = new ArrayList<>();
+
 		Cliente cliente = new Cliente(1L, "Cliente", "endereco 1", "login", "senha", pedidos);
 		Cliente cliente2 = new Cliente(2L, "Cliente2", "endereco 1", "login", "senha",pedidos);
+
 		Funcionario funcionario = new Funcionario(1L, "funcionario", "login", "senha",vendas);
 		Funcionario funcionario2 = new Funcionario(2L, "funcionario", "login", "senha",vendas);
 
+		Pedido pedido = new Pedido(1L, LocalDateTime.now(), "Preparando", cliente, produtos, vendas );
+		Pedido pedido2 = new Pedido(2L, LocalDateTime.now(), "Preparando", cliente, produtos, vendas );
 
 
+		// Cliente
 		Mockito.when(clienteRepository.save(cliente)).thenReturn(cliente);
 		Mockito.when(clienteRepository.save(cliente2)).thenReturn(cliente2);
 		//Mockito.when(clienteRepository.save(cliente3)).thenReturn(cliente3);
 		Mockito.when(clienteRepository.findById(1L)).thenReturn(Optional.of(cliente));
 
+		//Funcionario
 		Mockito.when(funcionarioRepository.save(funcionario)).thenReturn(funcionario);
 		Mockito.when(funcionarioRepository.save(funcionario2)).thenReturn(funcionario2);
 		//Mockito.when(funcionarioRepository.save(funcionario3)).thenReturn(funcionario3);
 		Mockito.when(funcionarioRepository.findById(1L)).thenReturn(Optional.of(funcionario));
+
+		//Pedido
+		Mockito.when(pedidoRepository.save(pedido)).thenReturn(pedido);
+		Mockito.when(pedidoRepository.save(pedido2)).thenReturn(pedido2);
+		Mockito.when(pedidoRepository.findById(1L)).thenReturn(Optional.of(pedido));
+		// Pizza
+
+		// Produto
+
+		// Venda
 
 	}
 
@@ -108,6 +133,22 @@ class PizzariaApplicationTests {
 		var funcionario = funcionarioController.findById(1L);
 		Assert.assertEquals(funcionario.getBody().getNome(), funcionarioController.findById(1L).getBody().getNome());
 	}
+
+	@Test
+	void testePedidoCriar(){
+		List<Pedido> pedidos = new ArrayList<>();
+		List<Venda> vendas = new ArrayList<>();
+		List<Produto> produtos = new ArrayList<>();
+		Cliente cliente = new Cliente(1L, "Cliente", "endereco 1", "login", "senha", pedidos);
+
+		PedidoDTO pedido = new PedidoDTO(1L, LocalDateTime.now(), "Preparando", cliente, produtos, vendas);
+	}
+
+	void testePedidoEditar(){}
+
+	void testePedidoDeletar(){}
+
+	void testePedidoFindById(){}
 
 
 }
